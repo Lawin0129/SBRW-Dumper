2e81ac6cf84e44a8df226ddccab297c517a21e72
Lawin0129 <56766256+Lawin0129@users.noreply.github.com>
2023-03-01 18:44:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ ReadLine.question("Would you like to dump your SBRW data? (y/n)\n", async (Answe
         options.forEach(a => optionString += ` [${a.index}] ${a.name}\n`);
 
         const option = await askQuestion(`\nSelect an option:\n${optionString}`);
+        
+        if (!options.find(x => x.index == Number(option)) {
+            console.log("ERROR: Not a valid option...\nClosing in 5 seconds...");
+            await sleep(5000);
+            process.exit(0);
+        }
 
         let servers = "";
         serverList.data.forEach(a => servers += ` [${a.index}] ${a.name}\n`);
